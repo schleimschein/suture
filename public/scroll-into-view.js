6e32b6a077abfdd0d6263344cd257ff7574d9573
schleimschein <sbstnstern@gmail.com>
2021-10-31 22:36:40 +0100

    Scroll highlight working

--- public/scroll-into-view.js
+++ public/scroll-into-view.js
@@ -1,21 +1,22 @@
 function initScrollIntoView() {
 
   var imgContainers_dict = {};
-  contentWrapper = document.getElementsByClassName('content-wrapper')[0];
-  imgContainers = contentWrapper.querySelectorAll('.img-container');
+  var contentWrapper = document.getElementsByClassName('content-wrapper')[0];
+  var imgContainers = contentWrapper.querySelectorAll('.img-container');
   imgContainers.forEach(imgContainer => {
     let name = imgContainer.dataset.artist;
     imgContainers_dict[name]=imgContainer;
   });
 
-  nameList = document.getElementsByClassName('name-list')[0];
-  nameElems = nameList.querySelectorAll('li');
+  var nameList = document.getElementsByClassName('name-list')[0];
+  var nameElems = nameList.querySelectorAll('li');
   nameElems.forEach(nameElem => {
     let name = nameElem.dataset.artist;
     if(name){
       let targetClick = nameElem.firstChild;
       let targetScroll = imgContainers_dict[name];
       targetClick.addEventListener("click", function() {
+        console.log(targetScroll);
         targetScroll.scrollIntoView({behavior: "smooth"});
       });
     }
